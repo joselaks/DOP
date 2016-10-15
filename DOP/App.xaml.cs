420d506c955a60979afc6c2a27ec02cfa2c644a5
JOSE <JOSE@JOSE>
2016-10-15 17:27:39 -0300

    Add project files.

---
+++ DOP/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace DOP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+           this.InitializeComponent();
+        }
+    }
+}
