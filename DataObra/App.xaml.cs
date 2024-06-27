4fa643866cc680af7722f9cf4a59ae1d726f1de4
joselaks <joselaks@gmail.com>
2024-06-27 19:20:18 -0300

    Agregar archivos de proyecto.

---
+++ DataObra/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DataObra
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            //Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzM1NDAzN0AzMjM2MmUzMDJlMzBLTnVMTDcyanFpbFRZa0RLNExzcVFGTnhOd0JadEU2U1VrRmNvdVY4bUhrPQ==");
+        }
+    }
+
+}
