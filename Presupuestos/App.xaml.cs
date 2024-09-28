740c658a88a6a6db596aa3ddd856cf496c2397f5
joselaks <joselaks@gmail.com>
2024-09-28 18:19:35 -0300

    Presupuestos y Gestión vacíos

---
+++ Presupuestos/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Presupuestos
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            //Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzUwMjU2MUAzMjM3MmUzMDJlMzBDZ3ZHalBiSHdOVTAybkNRWFlwako1ejJvQ0c5V245M0pSbktjVGRjNDRJPQ==");
+        }
+
+    }
+
+}
