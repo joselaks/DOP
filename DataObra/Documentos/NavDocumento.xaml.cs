26b7a5cd3d7f9f89c465a8f7fe753cfd2d78378c
sincommute@hotmail.com <sincommute@hotmail.com>
2024-10-04 14:07:39 -0300

    Arreglo objeto data

--- DataObra/Documentos/NavDocumento.xaml.cs
+++ DataObra/Documentos/NavDocumento.xaml.cs
@@ -21,9 +21,11 @@ namespace DataObra.Documentos
     {
         bool angosto = true;
         Servidor azure;
+        Datos.Conectores.ObtenerDocumentos oDatos;
         public NavDocumento(string pModo, string pDoc)
         {
             InitializeComponent();
+            oDatos = new Datos.Conectores.ObtenerDocumentos();
             this.FechaDesde.SelectedDate = DateTime.Today.AddDays(-30);
             this.FechaHasta.SelectedDate = DateTime.Today;
 
@@ -38,8 +40,7 @@ namespace DataObra.Documentos
 
         private async Task obtenerDocsAsync()
         {
-            Datos.Conectores.ObtenerDocumentos lista1 = new Datos.Conectores.ObtenerDocumentos();
-            this.GrillaDocumentos.ItemsSource = await lista1.ObtenerDocsAsync();
+            this.GrillaDocumentos.ItemsSource = await oDatos.ObtenerDocsAsync();
         }
 
         private void FichaWindow_DocumentoModified(object sender, Documento e)
