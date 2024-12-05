d488a31c944d0faa21f7b627bb90031a3359c869
sfabre <sebastianfabre@gmail.com>
2024-12-05 03:03:17 -0300

    Tiles

--- DataObra/Sistema/Principal.xaml.cs
+++ DataObra/Sistema/Principal.xaml.cs
@@ -1,12 +1,24 @@
-﻿using System.Windows;
+﻿using DataObra.Agrupadores;
+using DataObra.Agrupadores.Clases;
+using DataObra.Documentos;
+using DataObra.Insumos;
+using Syncfusion.Windows.Shared;
+using Syncfusion.Windows.Tools.Controls;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace DataObra.Sistema
 {
     public partial class Principal : Window
     {
+        public Tiles listaAgrupadores;
+        public Tiles listaDocumentos;
+        public Tiles listaInsumos;
+        public string selContenido;
+
         public Principal()
         {
             InitializeComponent();
@@ -57,6 +69,11 @@ namespace DataObra.Sistema
         {
             if (sender is Button botonPresionado)
             {
+                TilesDocumentos.Visibility = Visibility.Collapsed;
+                TilesAgrupadores.Visibility = Visibility.Collapsed;
+                TilesInsumos.Visibility = Visibility.Collapsed;
+                Panel.Visibility = Visibility.Collapsed;
+
                 // Restaurar el color de fondo de todos los botones
                 BotonPanel.Background = (SolidColorBrush)Resources["PanelBackground"];
                 BotonAgrupadores.Background = (SolidColorBrush)Resources["AgrupadoresBackground"];
@@ -68,7 +85,118 @@ namespace DataObra.Sistema
 
                 // Actualizar el contenido
                 string contenido = botonPresionado.Content.ToString();
-                AreaContenido.Content = new TextBlock { Text = contenido, FontSize = 24, HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
+                
+
+                if (contenido != null)
+                {
+                    selContenido = contenido;
+
+                    switch (contenido)
+                    {
+                        case "Documentos":
+                            if (listaDocumentos == null)
+                            {
+                                listaDocumentos = new Tiles();
+                                listaDocumentos.ListaString = new string[] { "Precostos", "Presupuestos", "Pedidos", "Planes", "Certificados", "Partes", "Facturas", "Compras", "Remitos", "Pagos", "Cobros", "Anticipos", "Contratos", "Sueldos", "Gastos", "Acopios" };
+
+                                foreach (var item in listaDocumentos.ListaString)
+                                {
+                                    TilesDocumentos.Items.Add(CrearTile(item, "Novedades sobre " + item + ".", "Turco"));
+                                }
+                            }
+                            TilesDocumentos.Visibility = Visibility.Visible;
+                            break;
+                        case "Agrupadores":
+                            if (listaAgrupadores == null)
+                            {
+                                listaAgrupadores = new Tiles();
+                                listaAgrupadores.ListaString = new string[] { "Obras", "Admin", "Clientes", "Proveedores", "Contratistas", "Obreros", "Cuentas", "Impuestos", "Depositos", "Temas" };
+
+                                foreach (string item in listaAgrupadores.ListaString)
+                                {
+                                    TilesAgrupadores.Items.Add(CrearTile(item, "Novedades sobre " + item + ".", "Naranja"));
+                                }
+                            }
+                            TilesAgrupadores.Visibility = Visibility.Visible;
+                            break;
+                        case "Insumos":
+                            if (listaInsumos == null)
+                            {
+                                listaInsumos = new Tiles();
+                                listaInsumos.ListaString = new string[] { "Materiales", "ManoDeObra", "Equipos", "SubContratos", "Otros", "Auxiliares", "Tareas", "Rubros", "Indices" };
+
+                                foreach (var item in listaInsumos.ListaString)
+                                {
+                                    TilesInsumos.Items.Add(CrearTile(item, "Novedades sobre " + item + ".", "Verde"));
+                                }
+                            }
+                            TilesInsumos.Visibility = Visibility.Visible;
+                            break;
+                        default:
+                            Panel.Visibility = Visibility.Visible;
+                            break;
+                }
+                }
+            }
+        }
+
+
+        private TileViewItem CrearTile(string pNombre, string pDescrip, string pColor)
+        {
+            TileViewItem nuevo = new TileViewItem();
+
+            TileChico contenido = new TileChico(pNombre, pDescrip);
+            nuevo.Content = contenido;
+
+            nuevo.Name = pNombre;
+            nuevo.MinMaxButtonVisibility = Visibility.Collapsed;
+            nuevo.HeaderVisibility = Visibility.Collapsed;
+            nuevo.MouseLeftButtonUp += ItemTile_Click;
+
+            switch (pColor)
+            {
+                case "Naranja":
+                    nuevo.Background = new SolidColorBrush(Colors.DarkOrange);
+                    break;
+                case "Violeta":
+                    nuevo.Background = new SolidColorBrush(Colors.Violet);
+                    break;
+                case "Verde":
+                    nuevo.Background = new SolidColorBrush(Colors.Green);
+                    break;
+                case "Azul":
+                    nuevo.Background = new SolidColorBrush(Colors.Blue);
+                    break;
+                case "Turqueza":
+                default:
+                    nuevo.Background = new SolidColorBrush(Colors.Turquoise);
+                    break;
+            }
+
+            return nuevo;
+        }
+
+
+        private void ItemTile_Click(object sender, MouseButtonEventArgs e)
+        {
+            TileViewItem tileSele = sender as TileViewItem;
+            bool abierto = false;
+
+            if (tileSele != null)
+            {
+                switch (selContenido)
+                {
+                    case "Documentos":
+                        //NavDocumento navegador = new NavDocumento("", "");
+
+                        break;
+                    case "Agrupadores":
+                        break;
+                    case "Insumos":
+                        break;
+                    default:
+                        break;
+                }
             }
         }
 
