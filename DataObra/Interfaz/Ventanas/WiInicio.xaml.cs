206466b6697f5a6a590a1dd41e9620dabfaa17a9
joselaks <joselaks@gmail.com>
2025-01-02 23:49:16 -0300

    Inicio y estructura Interfaz

---
+++ DataObra/Interfaz/Ventanas/WiInicio.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataObra.Interfaz.Ventanas
+{
+    /// <summary>
+    /// Lógica de interacción para WiInicio.xaml
+    /// </summary>
+    public partial class WiInicio : Window
+    {
+        public WiInicio()
+        {
+            InitializeComponent();
+            this.Loaded += WiInicio_Loaded;
+        }
+
+        private void WiInicio_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Crear y mostrar la ventana de login
+            WiLogin login = new WiLogin
+            {
+                Owner = this, // Establecer la ventana WiInicio como la propietaria de la ventana de login
+                WindowStartupLocation = WindowStartupLocation.CenterOwner // Centrar la ventana de login en WiInicio
+            };
+            login.ShowDialog();
+        }
+    }
+}
+
+
---
+++ DataObra/Interfaz/Ventanas/WiLogin.xaml.cs
@@ -0,0 +1,102 @@
+﻿using DataObra.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataObra.Interfaz.Ventanas
+{
+    /// <summary>
+    /// Lógica de interacción para WiLogin.xaml
+    /// </summary>
+    public partial class WiLogin : Window
+    {
+        public string Usuario { get; private set; }
+        public string Rol { get; private set; }
+
+        public ConsultasAPI InicioConsultasAPI;
+
+        public WiLogin()
+        {
+            InitializeComponent();
+            InicioConsultasAPI = new ConsultasAPI();
+
+            txtUsuario.Text = "jose@dataobra.com";
+            txtContraseña.Password = "contra";
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == "Usuario")
+            {
+                textBox.Text = "";
+            }
+        }
+
+        private async void VerificaUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            // Código a utilizar para la validación
+            var respuesta = await InicioConsultasAPI.ValidarUsuarioAsync(txtUsuario.Text, txtContraseña.Password);
+
+            if (respuesta.Success && respuesta.Usuario != null)
+            {
+                Usuario = respuesta.Usuario.Nombre;
+                Rol = "Compras"; // respuesta.Usuario.Rol;
+
+                this.DialogResult = true;
+                this.Close();
+            }
+            else
+            {
+                // Demo
+                Usuario = "Demo";
+                Rol = "Demo";
+
+                this.DialogResult = true;
+                this.Close();
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Usuario";
+            }
+        }
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            if (passwordBox.Password == "Contraseña")
+            {
+                passwordBox.Password = "";
+            }
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            if (string.IsNullOrWhiteSpace(passwordBox.Password))
+            {
+                passwordBox.Password = "Contraseña";
+            }
+        }
+
+        private void bSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
