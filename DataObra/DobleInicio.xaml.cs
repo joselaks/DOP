9ce0eb27a2607d13553772fe8cd386e9d8f20205
sincommute@hotmail.com <sincommute@hotmail.com>
2025-01-22 11:17:53 -0300

    doble inicio

---
+++ DataObra/DobleInicio.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+
+namespace DataObra
+{
+    /// <summary>
+    /// Lógica de interacción para DobleInicio.xaml
+    /// </summary>
+    public partial class DobleInicio : Window
+    {
+        public DobleInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Seba_Click(object sender, RoutedEventArgs e)
+        {
+            // Abrir la ventana Inicio.xaml
+            Inicio inicioWindow = new Inicio();
+            inicioWindow.Show();
+            this.Close();
+        }
+
+        private void Jose_Click(object sender, RoutedEventArgs e)
+        {
+            // Abrir la ventana WiInicio.xaml
+            Interfaz.Ventanas.WiInicio wiInicioWindow = new Interfaz.Ventanas.WiInicio();
+            wiInicioWindow.Show();
+            this.Close();
+        }
+    }
+}
