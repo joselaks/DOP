ab145bdbd6bce2e50879439c81624d08ec333120
joselaks <joselaks@gmail.com>
2025-02-12 14:05:38 -0300

    Get agrupadores

--- DataObra/Interfaz/Controles/UcAgrupador.xaml.cs
+++ DataObra/Interfaz/Controles/UcAgrupador.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataObra.Datos;
+using System;
 using System.Collections.Generic;
 using System.IO.Packaging;
 using System.Linq;
@@ -21,9 +22,65 @@ namespace DataObra.Interfaz.Controles
     /// </summary>
     public partial class UcAgrupador : UserControl
     {
-        public UcAgrupador(string Rol)
+        string Rol;
+
+        public UcAgrupador(string rol)
         {
             InitializeComponent();
+            Rol = rol;
+            //configuraRol(Rol);
+            CargarGrilla();
+
+        }
+
+        private async void CargarGrilla()
+        {
+            var AgrupadoresUsuario = await ConsultasAPI.ObtenerAgrupadoresPorCuentaID(App.IdCuenta);
+            this.GrillaAgrupadores.ItemsSource = AgrupadoresUsuario.agrupadores;
+        }
+
+        private void configuraRol(string rol)
+        {
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GrillaAgrupadores_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void actualizaGrilla_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Obra_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NuevoProveedor_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
