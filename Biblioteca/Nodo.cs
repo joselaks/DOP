71dea1ac53d039c28024dd5ad6353cdfe25188bf
joselaks <joselaks@gmail.com>
2025-03-20 18:44:54 -0300

    Nodo

--- Biblioteca/Nodo.cs
+++ Biblioteca/Nodo.cs
@@ -16,6 +16,7 @@ namespace Bibioteca.Clases
         private string tipo;
         private decimal cantidad;
         private decimal factor;
+        // Moneda 1
         private decimal pu;
         private decimal importe;
         private decimal materiales;
@@ -23,6 +24,14 @@ namespace Bibioteca.Clases
         private decimal equipos;
         private decimal subcontratos;
         private decimal otros;
+        // Moneda 2
+        private decimal pu2;
+        private decimal importe2;
+        private decimal materiales2;
+        private decimal manodeobra2;
+        private decimal equipos2;
+        private decimal subcontratos2;
+        private decimal otros2;
         private ObservableCollection<Nodo> inferiores;
 
         public bool Sup { get => sup; set { sup = value; OnPropertyChanged(nameof(Sup)); } }
@@ -33,6 +42,7 @@ namespace Bibioteca.Clases
         public string Tipo { get => tipo; set { tipo = value; OnPropertyChanged(nameof(Tipo)); } }
         public decimal Cantidad { get => cantidad; set { cantidad = value; OnPropertyChanged(nameof(Cantidad)); } }
         public decimal Factor { get => factor; set { factor = value; OnPropertyChanged(nameof(Factor)); } }
+        // Moneda 1
         public decimal PU { get => pu; set { pu = value; OnPropertyChanged(nameof(PU)); } }
         public decimal Importe { get => importe; set { importe = value; OnPropertyChanged(nameof(Importe)); } }
         public decimal Materiales { get => materiales; set { materiales = value; OnPropertyChanged(nameof(Materiales)); } }
@@ -40,6 +50,12 @@ namespace Bibioteca.Clases
         public decimal Equipos { get => equipos; set { equipos = value; OnPropertyChanged(nameof(Equipos)); } }
         public decimal Subcontratos { get => subcontratos; set { subcontratos = value; OnPropertyChanged(nameof(Subcontratos)); } }
         public decimal Otros { get => otros; set { otros = value; OnPropertyChanged(nameof(Otros)); } }
+        // Moneda 2 
+
+
+
+
+
         public bool HasItems => Inferiores != null && Inferiores.Count > 0;
 
         public Nodo Copia() => (Nodo)this.MemberwiseClone();
