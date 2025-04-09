a15a10cec093966943e9b5455795f6f67a3ed7a8
joselaks <joselaks@gmail.com>
2025-04-08 21:12:12 -0300

    Cambio DocumentoDTO

--- Biblioteca/DTO/DocumentoDTO.cs
+++ Biblioteca/DTO/DocumentoDTO.cs
@@ -7,32 +7,32 @@ using System.Threading.Tasks;
 namespace Biblioteca.DTO
 {
     public class DocumentoDTO
-    {
-        public int? ID { get; set; }
+        {
+        public int ID { get; set; }
         public short CuentaID { get; set; }
         public byte TipoID { get; set; }
         public int UsuarioID { get; set; }
-        public DateTime CreadoFecha { get; set; }
+        public DateTime CreadoFecha { get; set; } // Cambiar a DateOnly si es necesario
         public int? EditadoID { get; set; }
         public DateTime? EditadoFecha { get; set; }
-        public int? RevisadoID { get; set; }
-        public DateTime? RevisadoFecha { get; set; }
+        public int? AutorizadoID { get; set; } // Agregado
+        public DateTime? AutorizadoFecha { get; set; } // Agregado
         public int? AdminID { get; set; }
         public int? ObraID { get; set; }
         public int? PresupuestoID { get; set; }
         public int? RubroID { get; set; }
         public int? EntidadID { get; set; }
         public int? DepositoID { get; set; }
-        public string? Descrip { get; set; }
-        public string? Concepto1 { get; set; }
-        public DateTime Fecha1 { get; set; }
+        public string? Descrip { get; set; } // VARCHAR(150)
+        public string? Concepto1 { get; set; } // VARCHAR(150)
+        public DateTime Fecha1 { get; set; } // Cambiar a DateOnly si es necesario
         public DateTime? Fecha2 { get; set; }
         public DateTime? Fecha3 { get; set; }
-        public int? Numero1 { get; set; }
-        public int? Numero2 { get; set; }
-        public int? Numero3 { get; set; }
-        public string? Notas { get; set; }
-        public bool? Active { get; set; }
+        public int? Numero1 { get; set; } // Cambiado a nullable
+        public int? Numero2 { get; set; } // Cambiado a nullable
+        public int? Numero3 { get; set; } // Cambiado a nullable
+        public string? Notas { get; set; } // VARCHAR(250)
+        public bool Active { get; set; }
         public decimal Pesos { get; set; }
         public decimal Dolares { get; set; }
         public decimal Impuestos { get; set; }
@@ -54,6 +54,6 @@ namespace Biblioteca.DTO
         public bool RelRub { get; set; }
         public bool RelTar { get; set; }
         public bool RelIns { get; set; }
-    }
+        }
 
-}
+    }
