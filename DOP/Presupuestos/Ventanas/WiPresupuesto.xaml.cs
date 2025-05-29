38fb04e620fb446fe12bbb3bd012beae69cd6168
jose laks <joselaks@gmail.com>
2025-05-29 13:59:30 -0300

    Ribbon

---
+++ DOP/Presupuestos/Ventanas/WiPresupuesto.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Syncfusion.Windows.Tools.Controls;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DOP.Presupuestos.Controles;
+
+namespace DOP.Presupuestos.Ventanas
+{
+    /// <summary>
+    /// Lógica de interacción para WiPresupuesto.xaml
+    /// </summary>
+    public partial class WiPresupuesto : RibbonWindow
+    {
+        public WiPresupuesto()
+        {
+            InitializeComponent();
+            UcPresupuesto ucPresupuesto = new UcPresupuesto();
+            this.ppal.Children.Add(ucPresupuesto);  
+        }
+    }
+}
