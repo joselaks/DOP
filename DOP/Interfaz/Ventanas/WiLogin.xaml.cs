9e5c5361a26ba22b4f2a2a30a966826ada0b199a
joselaks <joselaks@gmail.com>
2025-08-15 23:19:58 -0300

    Ajuste

--- DOP/Interfaz/Ventanas/WiLogin.xaml.cs
+++ DOP/Interfaz/Ventanas/WiLogin.xaml.cs
@@ -42,7 +42,7 @@ namespace DOP.Interfaz.Ventanas
                 txtContraseña.Password = password;
                 }
             Inicio = inicio;
-            this.txtVersion.Text = "Versión 0.2005.2";
+            this.txtVersion.Text = "Versión 0.2005.3";
             }
 
 
