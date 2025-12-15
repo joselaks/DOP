da0ed0ce0fef352b92fa06449835434a29b4e395
joselaks <joselaks@gmail.com>
2025-12-14 21:16:08 -0300

    Ajustes

--- DOP/App.xaml.cs
+++ DOP/App.xaml.cs
@@ -93,9 +93,9 @@ namespace DOP
             // Aquí puedes decidir cuál URL usar, por ejemplo, basado en una configuración
 
             // cambiar cuando pase a local
-             BaseUrl = "https://localhost:5000/";
+            // BaseUrl = "https://localhost:5000/";
             // cambiar cuando pase a producción
-            // BaseUrl = "https://servidordataobra.azurewebsites.net/";
+            BaseUrl = "https://servidordataobra.azurewebsites.net/";
 
 
             }
